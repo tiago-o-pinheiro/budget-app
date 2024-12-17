c80f83f4b1232c075f0a13dbc318f3179c93ddc9
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-12-17 17:17:42 +0100

    [Dev] Create ModalProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Header } from "@components";
 import { Route, Routes } from "react-router-dom";
+import { Header, ModalProvider } from "@components";
+import { Accounts, Dashboard } from "@routes";
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <ModalProvider />
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/accounts/*" element={<Accounts />} />
       </Routes>
     </div>
   );
