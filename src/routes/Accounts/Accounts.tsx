1460236de322728a74ff0925b024046fab816676
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-12-19 13:46:16 +0100

    [Dev] WIP - Update route pages

--- src/routes/Accounts/Accounts.tsx
+++ src/routes/Accounts/Accounts.tsx
@@ -1,7 +1,9 @@
 import { AddAccount } from "@components";
 import { useAccountProvider, useModalProvider } from "@hooks";
+import { Account } from "@interfaces";
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
 
 const AccountsDashboard = () => {
   const { accounts } = useAccountProvider();
@@ -13,9 +15,108 @@ const AccountsDashboard = () => {
         <div key={account.id}>
           <p>{account.name}</p>
           <p>{account.balance}</p>
+
+          <div>
+            <Link to={`/accounts/${account.id}`}>Details</Link>
+          </div>
         </div>
       ))}
       <AddAccount />
+      <Link to="/">Return</Link>
+    </div>
+  );
+};
+
+interface AccountPageDetailsProps extends Omit<Account, "movements" | "id"> {}
+
+const AccountPageDetails = () => {
+  const { accountId } = useParams();
+  const { getAccount, editAccount } = useAccountProvider();
+  const account = getAccount(Number(accountId));
+  const navigate = useNavigate();
+  const { register, handleSubmit, setValue } = useForm<AccountPageDetailsProps>(
+    {
+      defaultValues: {
+        name: account?.name,
+        owner: account?.owner,
+        balance: account?.balance,
+        description: account?.description,
+        iban: account?.iban,
+        swift: account?.swift,
+      },
+    }
+  );
+
+  const onSubmit = (data: AccountPageDetailsProps) => {
+    editAccount(Number(accountId), data);
+    navigate("/accounts");
+  };
+
+  useEffect(() => {
+    if (!account) {
+      navigate(-1);
+    }
+
+    setValue("name", account?.name || "");
+    setValue("owner", account?.owner || "");
+    setValue("balance", account?.balance || 0);
+    setValue("description", account?.description || "");
+    setValue("iban", account?.iban || "");
+    setValue("swift", account?.swift || "");
+  }, [account]);
+
+  return (
+    <div>
+      <h4>Account Details</h4>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Account Name"
+          {...register("name", { required: true })}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Account owner"
+          {...register("owner", {})}
+        />
+        <br />
+        <br />
+        <input
+          type="number"
+          step={0.01}
+          placeholder="Balance"
+          {...register("balance", { required: true })}
+        />
+        <br />
+        <br />
+        <textarea
+          rows={4}
+          placeholder="Account description"
+          {...register("description", {})}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="IBAN"
+          {...register("iban", { pattern: /^[A-Z]{2}[0-9]{2}[A-Z0-9]+$/i })}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="BIC/SWIFT"
+          {...register("swift", {
+            pattern: /^[A-Z]{4}[A-Z]{2}[0-9A-Z]{2}([0-9A-Z]{3})?$/i,
+          })}
+        />
+        <br />
+        <br />
+        <input type="submit" />
+        <button onClick={() => navigate("/accounts")}>Cancel</button>
+      </form>
     </div>
   );
 };
@@ -33,7 +134,7 @@ export const Accounts = () => {
   return (
     <Routes>
       <Route path="/" element={<AccountsDashboard />} />
-      <Route path="/:accountId" element={<div>Account Id</div>} />
+      <Route path="/:accountId" element={<AccountPageDetails />} />
       <Route path="/new" element={<AddNewAccountPage />} />
     </Routes>
   );
--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -1,13 +1,33 @@
 import { AddMovement, Balance, Transactions } from "@components";
 import { useAccountProvider } from "@hooks";
+import { Movement } from "@interfaces";
 import { useEffect } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { AccountSelectorComponent } from "./components/AccountSelectorComponent";
+
+interface TransactionItemProps extends Movement {
+  account: string;
+}
 
 const DashboardContent = () => {
-  const { totalBalance, accounts } = useAccountProvider();
-
+  const {
+    totalBalance,
+    accounts,
+    getAllMovements,
+    getAccount,
+    getAccountMovement,
+  } = useAccountProvider();
+  const movements = getAllMovements() as TransactionItemProps[];
   const navigate = useNavigate();
   const location = useLocation();
+  const { accountId } = useParams();
+  const account = getAccount(parseInt(accountId ?? ""));
 
   useEffect(() => {
     if (accounts.length === 0) {
@@ -15,24 +35,47 @@ const DashboardContent = () => {
     }
   }, [accounts, navigate, location]);
 
-  if (accounts.length === 0) {
-    return null;
+  if (account && accountId) {
+    const movements = getAccountMovement(
+      parseInt(accountId)
+    ) as TransactionItemProps[];
+
+    return (
+      <div>
+        <Balance balance={account.balance} />
+        <Transactions movements={movements} />
+        <AddMovement id={account.id} />
+      </div>
+    );
   }
 
   return (
     <div>
       <Balance balance={totalBalance} />
-      <Transactions />
+      <Transactions movements={movements} />
       <AddMovement />
     </div>
   );
 };
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = event.target.value;
+    if (!selectedId || selectedId === "all") {
+      navigate(`/`);
+    } else {
+      navigate(`/${selectedId}`);
+    }
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<DashboardContent />} />
-      <Route path="/:accountId" element={<div>Account Id</div>} />
-    </Routes>
+    <div>
+      <AccountSelectorComponent handleSelectChange={handleSelectChange} />
+      <Routes>
+        <Route path="/:accountId?" element={<DashboardContent />} />
+      </Routes>
+    </div>
   );
 };
