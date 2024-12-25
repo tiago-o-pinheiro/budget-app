40d6708ffc4adaea7f632280c964feb9e502da36
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-12-25 12:32:07 +0100

    [Dev] Added theme to application

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
-import "@csstools/normalize.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
