3b32e14e6513ec54e9ab176939ece27dfe30326b
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-12-30 15:51:21 +0100

    [Dev] WIP Updated routes

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -1,4 +1,10 @@
-import { AddMovement, Balance, Transactions } from "@components";
+import {
+  AddMovement,
+  Balance,
+  Button,
+  Header,
+  Transactions,
+} from "@components";
 import { useAccountProvider } from "@hooks";
 import { Movement } from "@interfaces";
 import { useEffect } from "react";
@@ -10,10 +16,65 @@ import {
   useParams,
 } from "react-router-dom";
 import { AccountSelectorComponent } from "./components/AccountSelectorComponent";
+import {
+  ArrowsRightLeftIcon,
+  ChartBarSquareIcon,
+} from "@heroicons/react/24/outline";
+import { MovementDetails } from "./components/MovementDetails";
 
 interface TransactionItemProps extends Movement {
   account: string;
+  accountId: number;
 }
+
+interface DashboardContentProps {
+  accountId?: number;
+}
+const DashboardActions = ({ accountId }: DashboardContentProps) => {
+  return (
+    <div className="flex justify-around items-center">
+      <div className="flex flex-col items-center">
+        <AddMovement id={accountId} />
+      </div>
+      <div className="flex flex-col items-center">
+        <Button title="Exchange">
+          <ArrowsRightLeftIcon className="size-4 text-black" />
+        </Button>
+      </div>
+      <div className="flex flex-col items-center">
+        <Button title="Reports">
+          <ChartBarSquareIcon className="size-4 text-black" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+interface DashboardHeaderProps {
+  balance: number;
+  accountId?: number;
+}
+const DashboardHeader = ({ balance, accountId }: DashboardHeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = event.target.value;
+    if (!selectedId || selectedId === "all") {
+      navigate(`/`);
+    } else {
+      navigate(`/${selectedId}`);
+    }
+  };
+
+  return (
+    <div className="bg-white p-4 mt-8">
+      <Header />
+      <AccountSelectorComponent handleSelectChange={handleSelectChange} />
+      <Balance balance={balance} />
+      <DashboardActions accountId={accountId} />
+    </div>
+  );
+};
 
 const DashboardContent = () => {
   const {
@@ -42,40 +103,27 @@ const DashboardContent = () => {
 
     return (
       <div>
-        <Balance balance={account.balance} />
+        <DashboardHeader balance={account.balance} accountId={account.id} />
         <Transactions movements={movements} />
-        <AddMovement id={account.id} />
       </div>
     );
   }
 
   return (
     <div>
-      <Balance balance={totalBalance} />
+      <DashboardHeader balance={totalBalance} />
       <Transactions movements={movements} />
-      <AddMovement />
     </div>
   );
 };
 
 export const Dashboard = () => {
-  const navigate = useNavigate();
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedId = event.target.value;
-    if (!selectedId || selectedId === "all") {
-      navigate(`/`);
-    } else {
-      navigate(`/${selectedId}`);
-    }
-  };
-
   return (
-    <div>
-      <AccountSelectorComponent handleSelectChange={handleSelectChange} />
+    <>
       <Routes>
         <Route path="/:accountId?" element={<DashboardContent />} />
+        <Route path="/movement/*" element={<MovementDetails />} />
       </Routes>
-    </div>
+    </>
   );
 };
---
+++ src/routes/Dashboard/components/MovementDetails.tsx
@@ -0,0 +1,143 @@
+import {
+  Avatar,
+  Badge,
+  Button,
+  ConfirmDialog,
+  Container,
+  PageHeader,
+  Text,
+  Title,
+} from "@components";
+import { ArrowLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useAccountProvider, useCurrencyFormatter } from "@hooks";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const formatDate = (date: string) => {
+  return new Intl.DateTimeFormat("en-US", {
+    day: "2-digit",
+    month: "long",
+  }).format(new Date(date));
+};
+
+const MovementContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <PageHeader title="Movement Details" />
+      <Container styles="mt-10 bg-gray-100 rounded-3xl p-4 flex flex-col gap-1">
+        {children}
+      </Container>
+    </>
+  );
+};
+
+const Price = ({ value }: { value: number }) => {
+  const formattedValue = useCurrencyFormatter({ value });
+  return (
+    <Text
+      value={formattedValue}
+      size="md"
+      styles="font-bold"
+      color={value < 0 ? "red" : "green"}
+    />
+  );
+};
+
+export const MovementDetails = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [searchParams] = useSearchParams();
+
+  const accountId = searchParams.get("accountId");
+  const movementId = searchParams.get("movementId");
+  const { getAccount, removeMovement } = useAccountProvider();
+  const account = getAccount(Number(accountId));
+  const movement = account?.movements?.find((m) => m.id === Number(movementId));
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    if (!movement || !account?.name) return;
+    removeMovement(account.name, Number(movementId));
+    navigate(-1);
+    setIsOpen(false);
+  };
+
+  if (!movement)
+    return (
+      <MovementContainer>
+        <Title value="Movement not found" size="lg" styles="mb-4 text-center" />
+      </MovementContainer>
+    );
+
+  const date = formatDate(movement.date);
+
+  return (
+    <div>
+      <div className="flex flex-col justify-start items-center gap-4 mb-8 bg-gray-100 absolute top-0 left-0 right-0 p-4">
+        <div className="self-start gap-4" onClick={() => navigate(-1)}>
+          <ArrowLeftIcon className="w-6 h-6 text-black" />
+        </div>
+        <Avatar value={movement.name} />
+        <Title value={movement.name} size="lg" styles="" />
+        <Badge value={movement.category} />
+      </div>
+
+      <div className="flex justify-between items-center border-b-2 pb-4 mt-56">
+        <Text value={"Account name:"} size="md" color="primary" />
+        <Text
+          value={account?.name ?? ""}
+          size="sm"
+          color="primary"
+          styles="font-thin"
+        />
+      </div>
+
+      <div className="flex justify-between items-center mt-4 border-b-2 pb-4">
+        <Text value={"Amount:"} size="md" color="primary" />
+        <Price value={movement.value} />
+      </div>
+
+      <div className="flex justify-between items-center mt-4 border-b-2 pb-4">
+        <Text value={"Date:"} size="md" color="primary" />
+        <Text value={date} size="sm" styles="font-thin" />
+      </div>
+
+      {movement?.frequency && (
+        <div className="flex justify-between items-center mt-4 border-b-2 pb-4">
+          <Text value={"Frequency:"} size="md" color="primary" />
+          <Badge value={movement.frequency} />
+        </div>
+      )}
+      <div className="flex justify-between items-center mt-4 border-b-2 pb-4">
+        <Text value={"Category:"} size="md" color="primary" />
+        <Badge value={movement.category} />
+      </div>
+
+      {movement?.description && (
+        <div className="flex justify-between items-start mt-4 pb-4 flex-col gap-2">
+          <Text value={"Description:"} size="md" color="primary" />
+          <Text value={movement.description} size="sm" styles="font-thin" />
+        </div>
+      )}
+
+      <div className="text-right mt-4">
+        <Button
+          title="Delete"
+          family="danger"
+          size="md"
+          onClick={() => setIsOpen(true)}
+        >
+          <TrashIcon className="w-5 h-5 text-red-500" />
+        </Button>
+      </div>
+
+      {isOpen && (
+        <ConfirmDialog
+          title="Delete Movement"
+          text="Are you sure you want to delete this momvent? This action can't be undone."
+          cancelAction={() => setIsOpen(false)}
+          confirmAction={handleDelete}
+        />
+      )}
+    </div>
+  );
+};
