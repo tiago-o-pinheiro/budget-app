e6c234061f52de0ff9f66f1aa898131e04589183
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-01-02 18:42:47 +0100

    [Refactor] Added ListSelect to component

--- src/routes/Dashboard/components/AccountSelectorComponent.tsx
+++ src/routes/Dashboard/components/AccountSelectorComponent.tsx
@@ -1,69 +1,31 @@
-import { Field, Select } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import clsx from "clsx";
+import { Field } from "@headlessui/react";
+
 import { useAccountProvider } from "@hooks";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+
+import { ListSelect } from "@components";
 
 export const AccountSelectorComponent = ({
   handleSelectChange,
 }: {
-  handleSelectChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  handleSelectChange: (value: number) => void;
 }) => {
-  const [selectedAccount, setSelectedAccount] = useState<string>("all");
   const { getAccountArray } = useAccountProvider();
-  const accounts = getAccountArray()
-    .filter((account) => account !== undefined)
-    .map((account) => ({
-      ...account,
-      id: String(account.id),
-    }));
-  const { pathname } = useLocation();
+  const accounts = getAccountArray().filter((account) => account !== undefined);
 
-  const accountSelectorOptions =
-    accounts.length === 1
-      ? accounts
-      : [{ id: "all", name: "All Accounts" }, ...accounts];
+  const accountList = accounts.map((account) => ({
+    id: account.id,
+    name: account.name,
+  }));
 
-  useEffect(() => {
-    // If only one account, set it as default
-    if (accounts.length === 1) {
-      setSelectedAccount(accounts[0]?.id ?? "all");
-    } else {
-      const accountIdFromPath = pathname.split("/")[1] || "all";
-      setSelectedAccount(accountIdFromPath);
-    }
-  }, [pathname, accounts]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedAccount(e.target.value);
-    handleSelectChange(e);
+  const handleChange = (value: number) => {
+    handleSelectChange(value);
   };
 
   return (
     <div className="w-full max-w-64 px-4 mx-auto">
       <Field>
         <div className="relative">
-          <Select
-            className={clsx(
-              "mt-3 block w-full appearance-none bg-white/5 py-1.5 px-3 text-sm/6 text-gray",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
-              "border-b text-center rounded-none",
-              "*:text-black" // Windows-specific text fix
-            )}
-            onChange={handleChange}
-            value={selectedAccount}
-          >
-            {accountSelectorOptions.map((account) => (
-              <option key={account.id} value={account.id}>
-                {account.name}
-              </option>
-            ))}
-          </Select>
-          <ChevronDownIcon
-            className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-gray/60"
-            aria-hidden="true"
-          />
+          <ListSelect onClick={handleChange} options={accountList} />
         </div>
       </Field>
     </div>
