58dacf630dab99f7a4d83b8b764d821408bbb088
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-01-10 22:48:19 +0100

    [Fix] Added text size to component

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -1,16 +1,28 @@
 interface BadgeProps {
   value: string;
   color?: string;
+  size?: "xs" | "sm" | "md" | "lg";
 }
 
-export const Badge = ({ value, color }: BadgeProps) => {
+const TEXT_SIZE = {
+  xs: "text-xs/[8px]",
+  sm: "text-sm",
+  md: "text-base",
+  lg: "text-lg",
+};
+
+export const Badge = ({ value, color, size }: BadgeProps) => {
+  const displayValue =
+    size === "xs" && value.length > 20 ? `${value.slice(0, 17)}...` : value;
+
   return (
     <span
-      className={`text-xs rounded-full px-2 py-1 w-max capitalize ${
+      className={`rounded-full px-2 py-1 w-max capitalize ${
         color ? color : "bg-gray-200 text-gray-800"
-      }`}
+      } ${TEXT_SIZE[size || "md"]}`}
+      title={size === "xs" && value.length > 20 ? value : undefined}
     >
-      {value}
+      {displayValue}
     </span>
   );
 };
