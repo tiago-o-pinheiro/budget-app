17cada6f7cc704b28a7687f1573e429ad5aad5f5
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-01-13 12:58:26 +0100

    [Feat] Add insight route

---
+++ src/routes/Insights/Ingishts.tsx
@@ -0,0 +1,10 @@
+import { Container, PageHeader } from "@components";
+
+export const Insights = () => {
+  return (
+    <Container>
+      <PageHeader title="Insights" />
+      <h1>Insights</h1>
+    </Container>
+  );
+};
