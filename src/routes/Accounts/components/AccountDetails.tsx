c578353defbcf9569285a88c22e1e064160746be
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-01-15 11:44:46 +0100

    [Feat] Added account badge logic

--- src/routes/Accounts/components/AccountDetails.tsx
+++ src/routes/Accounts/components/AccountDetails.tsx
@@ -44,7 +44,11 @@ export const AccountDetails = () => {
     <Container>
       <PageHeader title={account.name} to="/accounts" />
       <div className="flex flex-col justify-center items-center gap-4 w-full">
-        <Badge value={"Main account"} size="xs" />
+        {account.type && !account.isMain ? (
+          <Badge value={account.type} size="xs" />
+        ) : null}
+
+        {account.isMain ? <Badge value={"Main account"} size="xs" /> : null}
         <Balance balance={account.balance} />
       </div>
       <Tabs />
