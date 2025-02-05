565f39954155f2bb5ffa8b49d6863b1ce41b4939
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-02-05 21:19:36 +0100

    [Refactor] Add Io5 icons to ICON_LIST for expanded icon options

--- src/config/constants/icon-list.constants.ts
+++ src/config/constants/icon-list.constants.ts
@@ -1,5 +1,6 @@
 import * as FaIcons from "react-icons/fa";
 import * as Fa6Icons from "react-icons/fa6";
+import * as IcoIcons5 from "react-icons/io5";
 
 const FA_ICONS = Object.keys(FaIcons)
   .filter((icon) => icon.startsWith("Fa"))
@@ -15,4 +16,11 @@ const FA6_ICONS = Object.keys(Fa6Icons)
     component: Fa6Icons[icon as keyof typeof Fa6Icons],
   }));
 
-export const ICON_LIST = [...FA_ICONS, ...FA6_ICONS];
+const ICO_ICONS_5 = Object.keys(IcoIcons5)
+  .filter((icon) => icon.startsWith("Io"))
+  .map((icon) => ({
+    name: icon,
+    component: IcoIcons5[icon as keyof typeof IcoIcons5],
+  }));
+
+export const ICON_LIST = [...FA_ICONS, ...FA6_ICONS, ...ICO_ICONS_5];
