283b0732a78301d1c10c0ef8cd9bd0895dd9949c
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-02-21 10:01:37 +0100

    [Refactor] Remove AddMovementForm and Exchange components for codebase cleanup

--- src/components/providers/ModalProvider/ModalProvider.tsx
+++ src/components/providers/ModalProvider/ModalProvider.tsx
@@ -1,11 +1,8 @@
-import { AddMovementForm } from "./forms/AddMovementForm";
 import { AccountForm } from "./forms/AccountForm";
 
 export const ModalProvider = () => {
   return (
     <div>
-      {/* <MovementForm /> */}
-      <AddMovementForm />
       <AccountForm />
     </div>
   );
